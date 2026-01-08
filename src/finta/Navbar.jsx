e5c087893a165b2fa4cad3df3083b79f85ed9821
Ashu Singh <ashusingh2201@gmail.com>
2026-01-08 23:21:04 +0530

    navbar completed

--- src/finta/Navbar.jsx
+++ src/finta/Navbar.jsx
@@ -1,5 +1,5 @@
 import Container  from './Container'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
   const navItems=[{title:"Founders",href:"/Founders"},
@@ -7,10 +7,12 @@ const Navbar = () => {
     {title:"Pricing",href:"/Pricing"},
     {title:"Log In",href:"/log in"}
   ]
-  return (
+
+  const [open,setOpen]=useState(false)
+  return (<>
     
       
-      <div className='flex justify-between px-10 md:px-50 items-center py-4 text-md bg-white '>
+      <div className='flex justify-between px-10 md:px-50 items-center py-4 text-md '>
         <div className='flex justify-between '>
           <img src='/finta-icon-light.svg' className='w-5'></img>
           <h2 className='text-[20px] font-semibold ml-1'>Finta</h2>
@@ -27,12 +29,39 @@ const Navbar = () => {
           
         </div>
 
-        <button className='md:hidden'>
+        <button className='md:hidden' onClick={()=>setOpen(!open)}>
           <i className='bx bx-menu'></i>
         </button>
         
       </div>
+      {open && (
+        <div className='fixed flex flex-col justify-between px-10 md:px-50 items-center py-4 text-md bg-white md:hidden rounded-md mx-3 w-[95%] top-2 shadow-strong'>
 
+        <div className='flex justify-between mb-5'>
+          <img src='/finta-icon-light.svg' className='w-5'></img>
+          <h2 className='text-[20px] font-semibold ml-1'>Finta</h2>
+
+        </div>
+        <div className=' gap-3 items-center flex flex-col'>
+          
+          {navItems.map((item)=>(
+            <a href={item.href} key={item.title} className='hover:text-neutral-500'>{item.title}</a>
+          ))}
+
+          <button className=' bg-blue-600 rounded-md px-2 py-1 text-white hover:bg-blue-800'>Get started</button>
+
+          
+        </div>
+
+        <button className='md:hidden absolute top-3 right-2' onClick={()=>setOpen(false)}>
+          <i className='bx bx-x bx-sm text-neutral-600 hover:text-neutral-400'></i>
+        </button>
+        
+      </div>
+        
+
+      )}
+      </>
  
     
   )
